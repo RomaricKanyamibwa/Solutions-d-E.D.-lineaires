4631a278e9a25cdda695c18f3565dbdd46050e4e
RomaricKanyamibwa <romaric.kanyamibwa@gmail.com>
2017-03-30 19:45:32 +0200

    Addition et Multiplication avec derivation
    On a choisit de implementer la methode de derivation pour eviter le calcule de suites

--- fonc_diff_finie/dfin_functions.py
+++ fonc_diff_finie/dfin_functions.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 14 14:44:25 2017
+@author: 3600594
+"""
+
 from ore_algebra import *
 from dfin_op import *
 from sage.calculus.var import var
@@ -10,6 +16,11 @@ def complete_coeff(diff_eq):
     This function returns a list with the coefficients of diff_eq
     -diff_eq is a differential of <class 'ore_algebra.ore_operator_1_1.UnivariateDifferentialOperatorOverUnivariateRing'>
     """
+    type_A=type(A("Dx"))
+    if(not(isinstance(diff_eq,type_A))):
+        raise ValueError,"Expected 1 argument of type ",type_A
+    if(len(diff_eq.coefficients())==diff_eq.order()+1):
+        return diff_eq.coefficients()
     L=[0]*(diff_eq.order()+1)
     mp=copy(diff_eq)
     while(mp!=0):
@@ -26,12 +37,15 @@ def calc_sum_func(L,CI,x):
     -CI is a list of numbers (Initial conditions at x)
     -x the point at which we evaluate the function
     """
-    print L
-    if(len(L)!=len(CI)+1):
+    #print L
+    if(len(L)<len(CI)+1):
         raise ValueError,"Incompatible length of L and CI .CI and L must have a length difference of 1"
     t=0
     for i in range(len(L)-1):
-        l=L[i](x)
+        if(isinstance( L[i], ( int, long,float,complex ) )):
+            l=L[i]
+        else:
+            l=L[i](x)
         t+=CI[i]*l
     return -t
 
@@ -40,94 +54,38 @@ def calc_init_con(diff_eq,n):
     Function that calculates the initial condition for Dx^n*f where f is the function that diff_eq describes
     diff_eq is of dfin_op type
     """
+    if(not(isinstance(diff_eq,dfin_op)) and not(isinstance(n,int))):
+        raise ValueError,"Expected 2 aguments of type dfin_op and int type"
     n=int(n)
     if(n<0):
         raise ValueError,"order is always a positive integer"
     length_IC=len(diff_eq.get_init_cond())
     if(length_IC>=n):
-        print "IC:",diff_eq.get_init_cond()[:n+1]
+        #print "IC:",diff_eq.get_init_cond()[:n+1]
         return diff_eq.get_init_cond()[:n+1]
     else:
-        d=copy(diff_eq.get_diff_eq())
-        Dx=DifferentialOperators()
-        d=A('Dx')*d
-        L=diff_eq.complete_coeff()
         CI=diff_eq.get_init_cond()
+        L=diff_eq.coefficients()
         CI=CI+[calc_sum_func(L,CI,diff_eq.get_x0())]
+        x0=diff_eq.get_x0()
+        #print "Coef:",L
+        #print "P(x)=",L[0]
+        #print "P("+str(x0)+")=",L[0](x0)
+        #print "Before add IC:",CI
+        d=copy(diff_eq.get_diff_eq())
         order_d=d.order()
-        print "Coef:",L
-        print "P(x)=",L[0]
-        print "P("+str(diff_eq.get_x0())+")=",L[0](diff_eq.get_x0())
-        L=complete_coeff(d)
-        print "Coef:",L
-        print "P(x)=",L[0]
-        print "P("+str(diff_eq.get_x0())+")=",L[0](diff_eq.get_x0())
-        print "Before add IC:",CI
-        CI=CI+[calc_sum_func(L,CI,diff_eq.get_x0())]
-        print "After add IC:",CI
-        
-        
-        
-def complete_init_cond(self,other,op_order):
-    """
-    This functions calculates the initial conditions using the reccurence series of the differential equation
-    """
-    self_init_cond_length=len(self.__init_cond)
-    other_init_cond_length=len(other.__init_cond)
-    if self_init_cond_length == other_init_cond_length :
-        i=0
-        newlist =[]
-        while(i< other_init_cond_length):
-            newlist.append(self.__init_cond[i] + other.__init_cond[i])
-            i += 1
-        l=op_order-len(newlist)
-        newlist=newlist+[0]*l
-        return newlist
-
-    elif (self_init_cond_length > other_init_cond_length) :
-        i = other_init_cond_length - self_init_cond_length
-        an=self.get_diff_eq().to_S(OreAlgebra(ZZ["n"], "Sn"))
-        an_order=an.order()
-        L=[0]*an_order
-        j=0
-        for k in self.get_init_cond():
-            L[j]=k
-            j+=1
-        order=other.__diff_eq.order()
-        L=an.to_list(L,order)
-        for j in range(i-1,order):
-            L[j]=factorial(j)*L[j]
-        i=0
-        newlist=[]
-        while (i < other_init_cond_length):
-            #other.__init_cond.append(...)
-            newlist.append(other.__init_cond[i] + L[i])
-            i += 1
-        l=op_order-len(newlist)
-        newlist=newlist+[0]*l
-        return newlist
-    
-    elif (self_init_cond_length < other_init_cond_length) :
-        i = other_init_cond_length - self_init_cond_length
-        an=self.get_diff_eq().to_S(OreAlgebra(ZZ["n"], "Sn"))
-        an_order=an.order()
-        L=[0]*an_order
-        j=0
-        for k in self.get_init_cond():
-            L[j]=k
-            j+=1
-        order=other.__diff_eq.order()
-        L=an.to_list(L,order)
-        for j in range(i-1,order):
-            L[j]=factorial(j)*L[j]
-        i=0
-        newlist=[]
-        while (i < other_init_cond_length):
-            #other.__init_cond.append(...)
-            newlist.append(other.__init_cond[i] + L[i])
-            i += 1
-        l=op_order-len(newlist)
-        newlist=newlist+[0]*l
-        return newlist
-    
-    
+        while(d.order()!=n):
+            d=A('Dx')*d
+            #print "Eq after derivation:",d
+            L=complete_coeff(d)
+            CI=CI+[calc_sum_func(L,CI,x0)]
+            #print "CI:",CI
+        #print "Coef:",L
+        #print "P(x)=",L[0]
+        #if(isinstance( L[0], ( int, long,float,complex ) )):
+            #print "P("+str(x0)+")=",L[0]
+        #else:
+            #print "P("+str(x0)+")=",L[0](x0)
+        #print "After add IC:",CI
+        return CI
+       
--- fonc_diff_finie/dfin_op.py
+++ fonc_diff_finie/dfin_op.py
@@ -7,6 +7,72 @@ Created on Tue Mar 14 14:44:25 2017
 from ore_algebra import *
 from sage.rings.integer_ring import ZZ
 from sage.functions.other import *
+from sage.all import *
+
+A = OreAlgebra(QQ['x'], 'Dx')
+
+def complete_coeff(diff_eq):
+    """
+    This function returns a list with the coefficients of diff_eq
+    -diff_eq is a differential of <class 'ore_algebra.ore_operator_1_1.UnivariateDifferentialOperatorOverUnivariateRing'>
+    """
+    type_A=type(A("Dx"))
+    if(not(isinstance(diff_eq,type_A))):
+        raise ValueError,"Expected 1 argument of type ",type_A
+    if(len(diff_eq.coefficients())==diff_eq.order()+1):
+        return diff_eq.coefficients()
+    L=[0]*(diff_eq.order()+1)
+    mp=copy(diff_eq)
+    while(mp!=0):
+        coef=mp.coefficients()
+        L[mp.order()]=coef[-1]
+        ch="Dx^"+str(mp.order())
+        d=A(ch)
+        mp=mp-L[mp.order()]*d
+    return L
+
+def calc_sum_func(L,CI,x):
+    """
+    -L is a list of functions
+    -CI is a list of numbers (Initial conditions at x)
+    -x the point at which we evaluate the function
+    """
+    if(len(L)<len(CI)+1):
+        raise ValueError,"Incompatible length of L and CI .CI and L must have a length difference of 1"
+    t=0
+    for i in range(len(L)-1):
+        if(isinstance( L[i], ( int, long,float,complex ) )):
+            l=L[i]
+        else:
+            l=L[i](x)
+        t+=CI[i]*l
+    return -t
+
+def calc_init_con(diff_eq,n):
+    """
+    Function that calculates the initial condition for Dx^n*f where f is the function that diff_eq describes
+    diff_eq is of dfin_op type
+    """
+    if(not(isinstance(diff_eq,dfin_op)) and not(isinstance(n,int))):
+        raise ValueError,"Expected 2 aguments of type dfin_op and int type"
+    n=int(n)
+    if(n<0):
+        raise ValueError,"order is always a positive integer"
+    length_IC=len(diff_eq.get_init_cond())
+    if(length_IC>n):
+        return diff_eq.get_init_cond()[:n+1]
+    else:
+        CI=diff_eq.get_init_cond()
+        L=diff_eq.coefficients()
+        CI=CI+[calc_sum_func(L,CI,diff_eq.get_x0())]
+        x0=diff_eq.get_x0()
+        d=copy(diff_eq.get_diff_eq())
+        order_d=d.order()
+        while(d.order()!=n):
+            d=A('Dx')*d
+            L=complete_coeff(d)
+            CI=CI+[calc_sum_func(L,CI,x0)]
+        return CI
 
 class dfin_op(object):
 
@@ -14,10 +80,12 @@ class dfin_op(object):
     différentielles linéaires
         -diff_eq est une equation differentiel
         -init_cond est une liste de conditions initiales de notre fonction
+        -et x0 est le point surlequel on definit les conditions initiales
     """
-
-    def __init__(self,diff_eq,init_cond=[]):
+    
+    def __init__(self,diff_eq,init_cond,x0=0):
         self.__diff_eq = diff_eq
+        self.__x0=x0
         if(init_cond!=[] and len(init_cond)>=diff_eq.order()):
             self.__init_cond=init_cond
         else:
@@ -25,6 +93,12 @@ class dfin_op(object):
 
     def get_diff_eq(self):
         return self.__diff_eq
+    
+    def get_x0(self):
+        return self.__x0
+    
+    def set_x0(self,x0):
+        self.__x0 = x0
 
     def set_init_cond(self,init_cond):
         self.__init_cond = init_cond
@@ -36,149 +110,67 @@ class dfin_op(object):
         self.__diff_eq = diff_eq
         
     def order(self):
-        return self.__diff_eq().order()
+        return self.__diff_eq.order()
 
     def degree(self):
         return self.__diff_eq.degree()
 
     def print_eq(self):
         print ("Dfinite equation:")
-        print ("Initiale Conditions:",self.__init_cond)
+        print ("Initiale Conditions at x0="+str(self.__x0)+":",self.__init_cond)
         print ("Equation:",self.__diff_eq)
+        
+    def __str__(self):
+        ch="("+str(self.__diff_eq)+","+str(self.__init_cond)+")"
+        return str(ch)
         
     def __eq__(self,other):
         return self.__diff_eq==other.get_diff_eq() and self.__init_cond==other.get_init_cond()
+    
+    def get_dfin_op(self):
+        return (self.__diff_eq,self.__init_cond)
+    
+    def coefficients(self):
+        """
+        This function returns a list with the coefficients of diff_eq
+        -diff_eq is a differential of <class 'ore_algebra.ore_operator_1_1.UnivariateDifferentialOperatorOverUnivariateRing'>
+        """
+        A = OreAlgebra(QQ['x'], 'Dx')
+        L=[0]*(self.__diff_eq.order()+1)
+        mp=copy(self.__diff_eq)
+        while(mp!=0):
+            coef=mp.coefficients()
+            L[mp.order()]=coef[-1]
+            ch="Dx^"+str(mp.order())
+            d=A(ch)
+            mp=mp-L[mp.order()]*d
+        return L
 
     def __add__(self,other):
         """Addition de 2 equa diff"""
-        self_init_cond_length=len(self.__init_cond)
-        other_init_cond_length=len(other.__init_cond)
-        if self_init_cond_length == other_init_cond_length :
-            i=0
-            newlist =[]
-            while(i< other_init_cond_length):
-                newlist.append(self.__init_cond[i] + other.__init_cond[i])
-                i += 1
-            z = dfin_op(self.__diff_eq.lclm(other.get_diff_eq()),newlist)
-            return z
+        newlist =[]
+        z0=self.__diff_eq.lclm(other.get_diff_eq())
+        n=z0.order()
+        L1=calc_init_con(self,n-1)
+        L2=calc_init_con(other,n-1)
+        i=0
+        while(i< n):
+            newlist.append(L1[i] + L2[i])
+            i += 1
+        z = dfin_op(z0,newlist,self.__x0)
+        return z
 
-        elif (self_init_cond_length > other_init_cond_length) :
-            i = self_init_cond_length - other_init_cond_length
-            an=other.get_diff_eq().to_S(OreAlgebra(ZZ["n"], "Sn"))
-            an_order=an.order()
-            L=[0]*an_order
-            j=0
-            for k in other.get_init_cond():
-                L[j]=k
-                j+=1
-            order=self.__diff_eq.order()
-            L=an.to_list(L,order)
-            for j in range(i-1,order):
-                L[j]=factorial(j)*L[j]
-            i=0
-            newlist=[]
-            while (i < self_init_cond_length):
-                #other.__init_cond.append(...)
-                newlist.append(self.__init_cond[i] + L[i])
-                i += 1
-            z0=self.__diff_eq.lclm(other.get_diff_eq())
-            l=(z0.order()-len(newlist))
-            newlist=newlist+[0]*l
-            z = dfin_op(z0,newlist)
-            return z
-        
-        elif (self_init_cond_length < other_init_cond_length) :
-            i = other_init_cond_length - self_init_cond_length
-            an=self.get_diff_eq().to_S(OreAlgebra(ZZ["n"], "Sn"))
-            an_order=an.order()
-            L=[0]*an_order
-            j=0
-            for k in self.get_init_cond():
-                L[j]=k
-                j+=1
-            order=other.__diff_eq.order()
-            L=an.to_list(L,order)
-            for j in range(i-1,order):
-                L[j]=factorial(j)*L[j]
-            i=0
-            newlist=[]
-            while (i < other_init_cond_length):
-                #other.__init_cond.append(...)
-                newlist.append(other.__init_cond[i] + L[i])
-                i += 1
-            z0=self.__diff_eq.lclm(other.get_diff_eq())
-            l=(z0.order()-len(newlist))
-            newlist=newlist+[0]*l
-            z = dfin_op(z0,newlist)
-            return z
 
     def __mul__(self,other):
         """Multiplication de 2 equa diff"""
-        self_init_cond_length=len(self.__init_cond)
-        other_init_cond_length=len(other.__init_cond)
-        if  self_init_cond_length ==  other_init_cond_length :
-            i=0
-            newlist =[]
-            while(i<  self_init_cond_length):
-                newlist.append(self.__init_cond[i] * other.__init_cond[i])
-                i+= 1
-            z = dfin_op(self.__diff_eq.symmetric_product(other.get_diff_eq()),newlist)
-
-            return z
-
-        elif (self_init_cond_length > other_init_cond_length) :
-            i = self_init_cond_length - other_init_cond_length
-            an=other.get_diff_eq().to_S(OreAlgebra(ZZ["n"], "Sn"))
-            an_order=an.order()
-            L=[0]*an_order
-            j=0
-            for k in other.get_init_cond():
-                L[j]=k
-                j+=1
-            order=self.__diff_eq.order()
-            L=an.to_list(L,order)
-            for j in range(i-1,order):
-                L[j]=factorial(j)*L[j]
-            i=0
-            newlist=[]
-            while (i < self_init_cond_length):
-                #other.__init_cond.append(...)
-                newlist.append(self.__init_cond[i] + L[i])
-                i += 1
-            z0=self.__diff_eq.symmetric_product(other.get_diff_eq())
-            l=(z0.order()-len(newlist))
-            newlist=newlist+[0]*l
-            z = dfin_op(z0,newlist)
-            return z
-        
-        elif (self_init_cond_length < other_init_cond_length) :
-            i = other_init_cond_length - self_init_cond_length
-            an=self.get_diff_eq().to_S(OreAlgebra(ZZ["n"], "Sn"))
-            an_order=an.order()
-            L=[0]*an_order
-            j=0
-            for k in self.get_init_cond():
-                L[j]=k
-                j+=1
-            order=other.__diff_eq.order()
-            L=an.to_list(L,order)
-            for j in range(i-1,order):
-                L[j]=factorial(j)*L[j]
-            i=0
-            newlist=[]
-            while (i < other_init_cond_length):
-                #other.__init_cond.append(...)
-                newlist.append(other.__init_cond[i] + L[i])
-                i += 1
-            z0=self.__diff_eq.symmetric_product(other.get_diff_eq())
-            l=(z0.order()-len(newlist))
-            newlist=newlist+[0]*l
-            z = dfin_op(z0,newlist)
-            return z
-            
-    def calc_init_con(self,n):
-        """Function that calculates the initial condition on an x0 point for the n-th order Dx"""
-        if(order<=0):
-            return []
-
-
+        newlist =[]
+        z0=z0=self.__diff_eq.symmetric_product(other.get_diff_eq())
+        n=z0.order()
+        L1=calc_init_con(self,n-1)
+        L2=calc_init_con(other,n-1)
+        i=0
+        while(i< n):
+            newlist.append(L1[i] * L2[i])
+            i += 1
+        z = dfin_op(z0,newlist,self.__x0)
+        return z
